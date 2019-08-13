732388e42a857dffe0f8f73935b8c0b4af5e9f4a
DanBehrman <dan.behrman@wework.com>
2019-08-13 09:28:27 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,51 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    all.map do |artist|
+      artist.years_experience
+    end.sum
+  end
+
+  def Artist.most_prolific
+    all.max_by do |artist|
+      artist.paintings.count
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,16 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Carl", 12)
+a2 = Artist.new("Marvin", 60)
 
+g1 = Gallery.new("The Met", "New York")
+g2 = Gallery.new("Big Museum", "Austin")
+
+p1 = Painting.new("The Car", 5000, a1, g1)
+p2 = Painting.new("Grandma", 60000, a2, g2)
+p3 = Painting.new("OH NO!", 13, a1, g2)
+p4 = Painting.new("Dad", 60000, a2, g1)
+p5 = Painting.new("Mom", 75000, a2, g2)
 
 
 binding.pry
